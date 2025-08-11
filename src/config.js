281b44ef12fdc8b9a0fa460050abca6a7f92d906
920039-collab <9.2003.9@mail.ru>
2025-08-11 12:16:27 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,7 @@
-export const API_URL = import.meta.env.VITE_API_URL || "https://YOUR-BACKEND.onrender.com";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
